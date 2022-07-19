f9179dd006449f1c5b1b7d3323c9e92f1feaafa4
Ivan Sinchuk <sinchuk_@hotmail.com>
2022-07-19 06:24:30 +0300

    changed width for 1440px

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,13 @@
-// const gulp         = require('gulp');
-// const browserSync  = require('browser-sync');
-// const sass         = require('gulp-sass')(require('sass'));
-// const cleanCSS     = require('gulp-clean-css');
-// const autoprefixer = require('gulp-autoprefixer');
-// const rename       = require("gulp-rename");
-
-// gulp.task('server', function() {
-
-//     browserSync({
-//         server: {
-//             baseDir: "src"
-//         }
-//     });
-
-//     gulp.watch("src/*.html").on('change', browserSync.reload);
-// });
-
-// gulp.task('styles', function() {
-//     return gulp.src("src/sass/**/*.+(scss|sass)")
-//         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-//         .pipe(rename({suffix: '.min', prefix: ''}))
-//         .pipe(autoprefixer())
-//         .pipe(cleanCSS({compatibility: 'ie8'}))
-//         .pipe(gulp.dest("src/css"))
-//         .pipe(browserSync.stream());
-// });
-
-// gulp.task('watch', function() {
-//     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-// });
-
-// gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
-
-
 "use strict";
 
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
-
 const sass         = require('gulp-sass')(require('sass'));
 const cleanCSS     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename       = require("gulp-rename");
-
+const ghpages      = require('gh-pages');
 
 const dist = "./dist/";
 // const dist = "C:/MAMP/htdocs/test/";
@@ -109,6 +73,7 @@ gulp.task("copy-img", () => {
                 .pipe(gulp.dest(dist + "/assets/img"))
                 .on("end", browsersync.reload);
 });
+
 
 
 gulp.task("watch", () => {
